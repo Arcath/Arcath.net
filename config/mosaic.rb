0a88165a2513ce856591b246a0d322dc2af78d5c
Arcath <adam@arcath.net>
2013-03-04 13:02:00 +0000

    mosaic blog post

--- config/mosaic.rb
+++ config/mosaic.rb
@@ -1,7 +1,7 @@
 # Moasic Provided Modules
 require 'mosaic/modules/static' # Static Content Handling
 require 'mosaic/modules/info' # Info Page (Comment out in production)
-require 'mosaic/modules/sass' # SASS Middleware needs (sass in your Gemfile)
+require 'mosaic/modules/sass' # SASS Middleware (needs sass in your Gemfile)
 
 # Require all modules in app/modules
 Dir.glob(Mosaic.root + '/app/modules/*', &method(:require))
